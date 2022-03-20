8fa2ff48b4ed9296d991c0e009c211e5e8be378f
Muhizi Mozaya <Muhizimozaya@gmail.com>
2022-03-21 01:16:06 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(function(){
+    $('#btn1').click(function(){
+    $('#cont').css({"background-color":"#191a14"});
+     });
+     $('#btn2').click(function(){
+    $('#cont').css({"background-color":" #eceecb"});
+     });
+
+     $('#btn5').click(function(){
+    $('#div-5').animate({right:'410px'},500);
+     });
+
+
+
+     $('#btn6').click(function(){
+    $('#div-5').animate({left:'578px'
+         
+
+ },500);
+     });
+
+     $('#btn3').click(function(){
+    $('#div-5').animate({bottom:'77px'
+         
+
+ },500);
+     });
+
+     $('#btn4').click(function(){
+    $('#div-5').animate({top:'65px'
+ },500);
+     });
+
+
+     $('#btn7').click(function(){
+    $('#div-5').animate({width:'31.5%',height:'400px'
+ },500);
+     });
+
+     });
